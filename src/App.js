2e277aac9e2461caa11fd8c7d788f715f32ac1a0
Felipe Chacon <felipipe88806@gmail.com>
2021-03-02 11:15:16 -0500

    [ADD] First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import react, {Fragment} from 'react'
+import { FaBeer, FaArrowLeft, FaPlus } from 'react-icons/fa';
+import img1 from "./assets/image/img1.png"
+import img2 from "./assets/image/img2.png"
+import img3 from "./assets/image/img3.png"
+import img4 from "./assets/image/img4.png"
+import img5 from "./assets/image/img5.png"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="container">
+        <div className="container-header">
+          <nav className="icon-header">
+            <FaArrowLeft />
+          </nav>
+          <div>
+            <h1 className="title">Helsinki</h1>
+            <p className="sub-title">Finland's southem capital sits on <br/> a peninsula in the Gulf of Finland.</p>
+          </div>
+        </div>
+        <div className="card">
+          <h2>Companions</h2>
+          <div className="card-image">
+            <img src={img1} alt="img1" />
+            <img src={img2} alt="img2" />
+            <img src={img3} alt="img3" />
+            <img src={img4} alt="img4" />
+            <img src={img5} alt="img5" />
+          </div>
+          <button className="card-button">+ Add New Companion</button>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
